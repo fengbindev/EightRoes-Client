2865c2da83d7e36f0a7f421412b1075035bd2888
奉斌 <947164239@qq.com>
2020-06-25 17:01:44 +0800

    头像接口调整

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -9,7 +9,7 @@
       <notice-icon class="action"/>
       <a-dropdown>
         <span class="action ant-dropdown-link user-dropdown-menu">
-          <a-avatar class="avatar" size="small" :src="avatar"/>
+          <a-avatar class="avatar" size="small" :src="avatar + userName"/>
           <span>{{ userName }}</span>
         </span>
         <a-menu slot="overlay" class="user-dropdown-menu-wrapper">
@@ -51,8 +51,13 @@ export default {
   components: {
     NoticeIcon
   },
+  data () {
+    return {
+      avatar: this.axios.defaults.baseURL + 'avatar/'
+    }
+  },
   computed: {
-    ...mapGetters(['userName', 'avatar'])
+    ...mapGetters(['userName'])
 
   },
   methods: {
